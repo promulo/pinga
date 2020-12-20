5f689e394a6e54a893efbd89691f209e678b6d9f
Paulo Barros <promuloo@gmail.com>
2020-12-20 07:06:38 +0100

    [GH-8] Add config loading/parsing (#9)

--- src/schema.py
+++ src/schema.py
@@ -1,3 +1,15 @@
+CONFIG_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "sites": {
+            "type": "array"
+        }
+    },
+    "required": ["sites"]
+}
+
+
 STATUS_SCHEMA = {
     "$schema": "http://json-schema.org/draft-07/schema#",
     "type": "object",
