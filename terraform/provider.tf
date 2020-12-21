3b257294435555aaef62a8bfdb888ab2027d303c
Paulo Barros <paulo.barros@crvsh.io>
2020-12-21 13:11:51 -0300

    Add Terraform resources for aiven account/project/team

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aiven = {
-      source = "aiven/aiven"
-      version = "2.1.2"
-    }
-  }
-}
-
 provider "aiven" {
   api_token = var.aiven_api_token
 }
---
+++ terraform/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    aiven = {
+      source = "aiven/aiven"
+      version = "2.1.2"
+    }
+  }
+  required_version = ">= 0.14"
+}
