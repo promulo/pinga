5aaa61a44f3247265b781557d25201296b0c462b
Paulo Barros <promuloo@gmail.com>
2020-12-21 11:55:18 -0300

    [GH-7] Add first version of site checker (+logging) (#11)

--- tests/test_pinga.py
+++ tests/test_pinga.py
@@ -1,29 +1,46 @@
 import pytest
 from jsonschema import SchemaError, ValidationError, validate
 from pinga import check_site
-from requests.exceptions import RequestException
 from schema import STATUS_SCHEMA
 
 
-def test_check_site():
+@pytest.mark.parametrize(
+    "url, status, http_status", [
+        ("http://example.org", "up", 200),
+        ("http://example.org/404", "down", 404),
+        ("http://example.org/500", "down", 500)
+    ]
+)
+def test_check_site_happy_path(url, status, http_status, requests_mock):
+    requests_mock.get(url, status_code=http_status)
+
+    result = check_site(url)
+
     try:
-        validate(instance=check_site('https://example.org'), schema=STATUS_SCHEMA)
+        validate(instance=result, schema=STATUS_SCHEMA)
     except (SchemaError, ValidationError) as err:
         pytest.fail("Unexpected validation error: {}".format(err))
+    assert result["url"] == url
+    assert result["status"] == status
+    assert result["httpStatus"] == http_status
 
 
 @pytest.mark.parametrize(
-    "sample, expected_msg", [
+    "url, expected_msg", [
         ("foo", "Invalid URL"),
         ("", "Invalid URL"),
         ("a", "Invalid URL"),
-        (None, "Invalid URL"),
-        (1, "Invalid URL"),
+        ("1", "Invalid URL"),
         ("http://localhost:9999", "Max retries exceeded with url")
     ]
 )
-def test_check_site_error(sample, expected_msg):
-    with pytest.raises(RequestException) as exc:
-        validate(instance=check_site(sample), schema=STATUS_SCHEMA)
+def test_check_site_error(url, expected_msg):
+    result = check_site(url)
 
-    assert expected_msg in str(exc.value)
+    try:
+        validate(instance=result, schema=STATUS_SCHEMA)
+    except (SchemaError, ValidationError) as err:
+        pytest.fail("Unexpected validation error: {}".format(err))
+    assert result["url"] == url
+    assert result["status"] == "error"
+    assert expected_msg in result["errorMessage"]
