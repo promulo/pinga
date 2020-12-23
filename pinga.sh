e1c18366b118a5aff90f5db32da33ee9918a2cc8
Paulo Barros <paulo.barros@crvsh.io>
2020-12-23 16:45:48 -0300

    Add systemd files and pinga.sh script

---
+++ pinga.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+install_dependencies() {
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip3 install -r /opt/pinga/requirements.txt
+}
+
+cd /opt/pinga
+
+if [ $# -lt 1 ]; then
+    echo "FATAL: No parameter supplied. Usage: ./pinga.sh producer|consumer"
+    exit 1
+elif [ $1 == "producer" ]; then
+    install_dependencies
+    .venv/bin/python /opt/pinga/run_producer.py
+elif [ $1 == "consumer" ]; then
+    install_dependencies
+    .venv/bin/python /opt/pinga/run_consumer.py
+else
+    echo "FATAL: Unknown option ${1}. Valid options are: producer|consumer"
+fi
